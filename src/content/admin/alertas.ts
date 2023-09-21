1582ed9581dfdcb9aab86a50274fbb1f7aea665d
LAPCEN <142829643+lapcenUnB@users.noreply.github.com>
2023-09-21 20:00:14 -0300

    Update alertas.ts

--- src/content/admin/alertas.ts
+++ src/content/admin/alertas.ts
@@ -1,9 +1,4 @@
 export const alertas: AlertsT = [
-  {
-    texto_destaque: 'Até 28 / 08',
-    texto_conteudo: 'Processo seletivo aberto! »',
-    redireciona_para: '/processo-seletivo20232',
-  },
   // {
   //   texto_destaque: 'Outro alerta',
   //   texto_conteudo: 'Processo seletivo2 aberto! »',
